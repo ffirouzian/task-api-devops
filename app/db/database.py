9ea1dd06b7ddd74e54500ee62106bca3b1c1b374
Zahra <zffirouzian@gmail.com>
2026-08-14 03:52:25 +0000

    fix ruff import checks

--- app/db/database.py
+++ app/db/database.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-
 load_dotenv(override=False)
 
 
--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,5 +1,5 @@
 from app.db.database import Base, engine
-from app.models.task import Task
+from app.models.task import Task  # noqa: F401
 
 
 def init_db():
